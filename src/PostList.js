d994776a5ab09b5ecbd36af71d47258278594c0d
ChotikaLuangorachorn <chotika.lu@ku.th>
2018-07-22 14:22:10 +0700

    search

--- src/PostList.js
+++ src/PostList.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 class PostList extends Component {
   constructor(props) {
     super(props)
-    this.state = { list: [] }
-
-
+    this.state = { list: [], title: "" }
   }
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/posts/')
@@ -17,18 +15,43 @@ class PostList extends Component {
         this.setState({ list: json })
       })
   }
+  handleChange = (e) => {
+    this.setState({ title: e.target.value });
+    e.preventDefault();
+  }
+
+  onSubmitSearch = (e) => {
+
+    const { title } = this.state;
+
+    let url = 'https://jsonplaceholder.typicode.com/posts'
+    if (title !== '') {
+      url += `?title=${title}`;
+    }
+    console.log(url)
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((json) => {
+        console.log(json)
+        this.setState({ list: json })
+      })
+    e.preventDefault();
+  }
 
 
   render() {
     return (
       <div>
         <h2>Posts</h2>
-        <form>
-          <input/>
-          <button>
+        <form onSubmit={this.onSubmitSearch}>
+          <input name="title" type="text" value={this.state.title} onChange={this.handleChange} />
+          <button type="submit">
             Search
           </button>
         </form>
+
         <ol style={{ 'textAlign': 'left' }}>
           {
             this.state.list.map((data, index) => (
